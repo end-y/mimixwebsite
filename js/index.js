26ff27991e50474d25349772ba66cd27ad310a3f
ender yazici <enderyazici57@gmail.com>
2021-01-29 13:42:45 +0300

    some_changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,30 @@
-AOS.init();
-particlesJS.load('particles-js', 'assets/particles.json', function() {});
-document.getElementById("accept").addEventListener("click", () => {
-    document.getElementById("cardie").classList.add("fade")
-})
+$(document).ready(() => {
+    if(localStorage.getItem("accept") != "true"){
+        $("#cardie").css("display", "inline")
+    }
+    
+    AOS.init();
+    
+    particlesJS.load('particles-js', 'assets/particles.json', () => {});
+    
+    $("#accept").click(()=> {
+        $("#cardie").addClass("fade")
+        $("#cardie").css("display", "none")
+        localStorage.setItem("accept","true");
+    })
+
+    let scrollAnime = () => {
+        var v2 = Math.abs($('#bizeulasin').position().top - $(window).height()/2);
+        var v1 = $(this).scrollTop();
+            if( v1 > v2 ){
+                $(".img1").addClass("anim1")
+                $(".img2").addClass("anim2")
+                $(".img3").addClass("anim3")
+            }
+    }
+    
+    $(document).scroll((evt) => {
+        scrollAnime()
+    });
+    scrollAnime()
+});
